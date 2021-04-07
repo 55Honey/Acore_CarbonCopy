68aa0fdc22744998ee8a1d0f32c1596550846241
55Honey <71938210+55Honey@users.noreply.github.com>
2021-04-07 18:04:36 +0200

    Add more characters to remove with gsub

--- CarbonCopy.lua
+++ CarbonCopy.lua
@@ -74,10 +74,8 @@ CharDBQuery('CREATE TABLE IF NOT EXISTS `'..Config.customDbName..'`.`carboncopy`
 local function CopyCharacter(event, player, command)
 
     local commandArray = cc_splitString(command)
-    commandArray[2] = commandArray[2]:gsub(" ", "")
-    commandArray[2] = commandArray[2]:gsub(";", "")
-    commandArray[3] = commandArray[3]:gsub(" ", "")
-    commandArray[3] = commandArray[3]:gsub(";", "")
+    commandArray[2] = commandArray[2]:gsub("[';\\, ]", "")
+    commandArray[3] = commandArray[3]:gsub("[';\\, ]", "")
     if commandArray[1] == "carboncopy" then
         if player == nil then
             print("This command con not be run from the console, but only from the character to copy.")
